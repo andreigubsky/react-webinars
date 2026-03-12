40e55c430afaacecf721c0fe51985a44f519d53c
Andrew Gubsky <andrei.gubsky@gmail.com>
2026-03-12 18:28:57 +0200

    Modal window

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import Main from "./Main"
 import Footer from "./Footer"
 import ClickCounter from "./ClickCounter";
 import CountDisplay from "./CountDisplay";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import TagWidget from "./TagWidget";
 import Dates from "./Dates";
@@ -17,6 +17,8 @@ import ArticleList from "./ArticleList";
 import type { Article } from "../types/article";
 import { fetchArticles } from "../services/articleService";
 import HtmlFor from "./HtmlFor";
+import axios from "axios";
+import Modal from "./Modal";
 
 
 export default function App () {
@@ -29,7 +31,47 @@ export default function App () {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
 
-    const handleClick = () => {
+    const [person, setPerson] = useState(null);
+    const [count, setCount] = useState(1);
+    const [time, setTime] = useState(new Date());
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+    
+    useEffect(()=>{
+        console.log(`Effect ran for: ${count}`);
+        // axios
+        //         .get(`https://swapi.info/api/people/${count}`)
+        //     .then((response) => setPerson(response.data));
+
+        // console.log('App rendred!');
+
+        async function fetchData() {
+            const response = await axios.get(`https://swapi.info/api/people/${count}`);
+            setPerson(response.data)
+        }
+        fetchData();
+        console.log('App rendred!');
+        return () => {
+            console.log(`Clean up for ${count}`);
+          };
+    },[count]);
+    
+   useEffect(()=>{
+    const intervalId = setInterval(() => {
+        setTime(new Date());
+        console.log(`Interval - ${Date.now()}`);
+        return () => {
+            clearInterval(intervalId);
+            console.log(`Clean up for ${count}`);
+          };
+      }, 1000);
+   },[])
+    
+        const handleClick = () => {
         setClick(clicks+1)
         console.log(clicks);
     };
@@ -65,6 +107,20 @@ export default function App () {
                 </button><br/>
                 {isOpen &&
                 <>
+                <h1>Main content of the page</h1>
+                <button onClick={openModal}>Open modal</button>
+                {isModalOpen && (
+                    <Modal onClose={closeModal}>
+                    <h2>Custom Modal Content</h2>
+                    <p>This is a reusable modal with dynamic content.</p>
+                  </Modal>
+                )}
+                <h2>The count is {count}</h2>
+                <button onClick={() => setCount(count + 1)}>Get next character</button>
+                <p>TimerBox - {time.toLocaleTimeString()}</p>
+                <pre>{JSON.stringify(person, null, 2)}</pre>
+                
+
                 <HtmlFor />
                 <SearchForm onSubmit={handleSearch} />
                 {isLoading && <p>Loading data, please wait...</p>}
